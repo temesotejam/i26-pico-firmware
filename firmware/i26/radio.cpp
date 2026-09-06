4acafa62789f5bbff6bd9cd93cf94141b91db025
AIAAOOAO <159702851+temesotejam@users.noreply.github.com>
2026-09-06 13:28:11 +0900

    プロポからの値を見れないようにした。

--- firmware/i26/radio.cpp
+++ firmware/i26/radio.cpp
@@ -75,19 +75,19 @@ void on_uart_rx(void) {
         switch(chars_rxed){
             case 3:
                 Chdata[0]=(sbus_data[1]|(sbus_data[2]<<8)&0x07ff);
-                printf("%04d ",Chdata[0]);
+                // printf("%04d ",Chdata[0]);
                 break;
             case 4:
                 Chdata[1]=(sbus_data[3]<<5|sbus_data[2]>>3)&0x07ff;
-                printf("%04d ",Chdata[1]);
+                // printf("%04d ",Chdata[1]);
                 break;
             case 6:
                 Chdata[2]=(sbus_data[3]>>6|sbus_data[4]<<2|sbus_data[5]<<10)&0x07ff;
-                printf("%04d ",Chdata[2]);
+                // printf("%04d ",Chdata[2]);
                 break;
             case 7:
                 Chdata[3]=(sbus_data[6]<<7|sbus_data[5]>>1)&0x07ff;
-                printf("%04d ",Chdata[3]);
+                // printf("%04d ",Chdata[3]);
                 break;
             case 8:
                 Chdata[4]=(sbus_data[7]<<4|sbus_data[6]>>4)&0x07ff;
